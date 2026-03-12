d496d19e0ba7d97076d060ce6828f6121fd24869
Abhijit P <pabhijit047@gmail.com>
2026-03-12 14:36:57 +0530

    Call Polyfill

---
+++ Class-7/call.js
@@ -0,0 +1,16 @@
+// Polyfill for call method
+
+Function.prototype.myCall = function (context, ...args) {
+  context.tempFn = this;
+  const result = context.tempFn(...args);
+  delete context.tempFn;
+  return result;
+};
+function sayHello(city, country) {
+  console.log(`Hello ${this.name} ${city} ${country}`);
+}
+let ob1 = {
+  name: "Steve",
+};
+
+sayHello.myCall(ob1, "mumbai", "India");
